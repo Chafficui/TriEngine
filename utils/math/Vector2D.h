8b11ee99fa195ea88e43d936511e71b05469c969
Chafficui <felixbeinssen@gmail.com>
2023-03-12 14:27:21 +0000

    [tri01] feat: implemented add,sub,mult and div functions

---
+++ utils/math/Vector2D.h
@@ -0,0 +1,44 @@
+//
+// Created by felix on 12.03.2023.
+//
+
+#ifndef TRIENGINE_VECTOR2D_H
+#define TRIENGINE_VECTOR2D_H
+
+
+class Vector2D {
+public:
+    float x;
+    float y;
+
+    Vector2D();
+
+    Vector2D(float x, float y);
+
+    Vector2D &add(const Vector2D &vec);
+
+    Vector2D &subtract(const Vector2D &vec);
+
+    Vector2D &multiply(const Vector2D &vec);
+
+    Vector2D &divide(const Vector2D &vec);
+
+    friend Vector2D &operator+(Vector2D &v1, const Vector2D &v2);
+
+    friend Vector2D &operator-(Vector2D &v1, const Vector2D &v2);
+
+    friend Vector2D &operator*(Vector2D &v1, const Vector2D &v2);
+
+    friend Vector2D &operator/(Vector2D &v1, const Vector2D &v2);
+
+    Vector2D &operator+=(const Vector2D &vec);
+
+    Vector2D &operator-=(const Vector2D &vec);
+
+    Vector2D &operator*=(const Vector2D &vec);
+
+    Vector2D &operator/=(const Vector2D &vec);
+};
+
+
+#endif //TRIENGINE_VECTOR2D_H
